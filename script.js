c0f7801455ea233e96a933f4a370cce16d2a59b1
TJ <31156954+advane@users.noreply.github.com>
2021-11-03 22:25:52 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Text animation on page load
+document.addEventListener('DOMContentLoaded', function (event) {
+
+    dataText = [
+		["title", "MARSWALKS HECK EVERY CYPHER TOOLKIT"],
+		["subtitle", "Hello from Belgium, "],
+		["subtitle", "This home page is kinda plagarised from some random Github site lol,"],
+		["subtitle", "Hecking by Marswalk,"],
+		["subtitle", "And special thanks to pheonix121,"],
+		["subtitle", "and bf sam for emotional support"],
+  ]
+
+  startTextAnimation(0)
+
+})
