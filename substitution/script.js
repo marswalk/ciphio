b3383add24a6ceb216cb0c2093542ad695283602
Timmy Wen <oh.tingwen@gmail.com>
2022-12-03 12:00:00 +0000

    Completely rewrite Substitution cipher and Hill Climb UI

--- substitution/script.js
+++ substitution/script.js
@@ -1,42 +1,48 @@
-let quadgrams = {}; // Maps 4-letter sequences to their log probability score
-let quadgramsLoaded = false;
-let evolutionRunning = false; // Flag to control the run state of the hill climbing loop
-let bestGlobalScore = -Infinity; // Tracks the highest score found across all rounds
+let ngrams = {}; 
+let currentNgramSize = 4;
+let ngramsLoaded = false;
+let evolutionRunning = false; 
+let bestGlobalScore = -Infinity; 
 let bestGlobalKey = "";
 let bestGlobalDecryption = "";
 
-// Fetch the pre-computed quadgram scores from the CSV file
-fetch('4gramScores.csv')
-    .then(response => response.text())
-    .then(text => {
-        const lines = text.split('\n');
-        for (let line of lines) {
-            const parts = line.trim().split(' ');
-            if (parts.length >= 3) {
-                // Parse the 4-gram and its log probability score
-                quadgrams[parts[0]] = parseInt(parts[2]);
+function loadNgrams(size, callback) {
+    ngramsLoaded = false;
+    ngrams = {};
+    currentNgramSize = size;
+    
+    fetch(`${size}gramScores.csv`)
+        .then(response => response.text())
+        .then(text => {
+            const lines = text.split('\n');
+            for (let line of lines) {
+                const parts = line.trim().split(' ');
+                if (parts.length >= 3) {
+                    ngrams[parts[0]] = parseInt(parts[2]);
+                }
             }
-        }
-        quadgramsLoaded = true;
-    });
+            ngramsLoaded = true;
+            if (callback) callback();
+        })
+        .catch(err => {
+            console.error("Failed to load n-grams", err);
+        });
+}
 
-// Evaluates the fitness of a piece of text based on english quadgram frequencies
-function quadgramScore(text) {
+function ngramScore(text) {
     let score = 0;
-    // Iterate over every 4-letter sequence in the text
-    for (let i = 0; i < text.length - 3; i++) {
-        const quad = text.substring(i, i + 4);
-        if (quadgrams[quad]) {
-            score += quadgrams[quad];
+    const size = currentNgramSize;
+    for (let i = 0; i < text.length - (size - 1); i++) {
+        const seq = text.substring(i, i + size);
+        if (ngrams[seq]) {
+            score += ngrams[seq];
         } else {
-            // Penalize heavily for invalid or rare sequences
             score -= 3000;
         }
     }
     return score;
 }
 
-// Decodes a substitution cipher text using a provided alphabet key mapping
 function decodeSubstitution(text, key) {
     const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let decoded = "";
@@ -44,22 +50,18 @@ function decodeSubstitution(text, key) {
         const char = text[i];
         const idx = key.indexOf(char);
         if (idx !== -1) {
-            // Map the ciphertext character to the plaintext character based on index
             decoded += alphabet[idx];
         } else {
-            // Preserve non-alphabetic characters
             decoded += char;
         }
     }
     return decoded;
 }
 
-// Removes all non-alphabetic characters and uppercases the text for analysis
 function getCleanText(text) {
     return text.toUpperCase().replace(/[^A-Z]/g, '');
 }
 
-// Randomizes the order of elements in an array (Fisher-Yates shuffle)
 function shuffle(array) {
     let currentIndex = array.length, randomIndex;
     while (currentIndex !== 0) {
@@ -70,85 +72,115 @@ function shuffle(array) {
     return array;
 }
 
-// Initiates the Hill Climbing search for the substitution cipher key
 function startHillClimb() {
-    if (!quadgramsLoaded) return;
     const form = document.forms["evolutionForm"];
     const message = form["message"].value;
     const rounds = parseInt(document.getElementById("roundinput").value) || 5;
+    const maxTries = parseInt(document.getElementById("maxTriesInput").value) || 2000;
+    const selectedNgram = parseInt(document.getElementById("ngramSelect").value) || 4;
+
     if (!message) return;
 
-    // Toggle UI state
     document.getElementById("startEvolutionButton").disabled = true;
     document.getElementById("stopEvolutionButton").disabled = false;
-    evolutionRunning = true;
     
-    // Reset global tracking variables
-    bestGlobalScore = -Infinity;
-    bestGlobalKey = "";
-    bestGlobalDecryption = "";
+    // Clear previous results
+    document.getElementById("evolutionResult").innerHTML = "";
 
-    const cleanText = getCleanText(message);
-    const startTime = Date.now();
-    let currentRound = 0;
+    loadNgrams(selectedNgram, () => {
+        evolutionRunning = true;
+        bestGlobalScore = -Infinity;
+        bestGlobalKey = "";
+        bestGlobalDecryption = "";
 
-    // Execute one round of Hill Climbing, utilizing requestAnimationFrame to avoid freezing the browser
-    function runRound() {
-        if (!evolutionRunning || currentRound >= rounds) {
-            document.getElementById("startEvolutionButton").disabled = false;
-            document.getElementById("stopEvolutionButton").disabled = true;
-            evolutionRunning = false;
-            return;
+        const cleanText = getCleanText(message);
+        const startTime = Date.now();
+        let currentRound = 0;
+        let key, bestScore, bestKey, ctr, totalIterations;
+
+        function startRound() {
+            if (!evolutionRunning || currentRound >= rounds) {
+                document.getElementById("startEvolutionButton").disabled = false;
+                document.getElementById("stopEvolutionButton").disabled = true;
+                evolutionRunning = false;
+                return;
+            }
+
+            key = shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""));
+            bestScore = ngramScore(decodeSubstitution(cleanText, key.join("")));
+            bestKey = [...key];
+            ctr = 0;
+            totalIterations = 0;
+            
+            requestAnimationFrame(runIterations);
         }
 
-        // Start with a completely randomized alphabet key
-        let key = shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""));
-        let bestScore = quadgramScore(decodeSubstitution(cleanText, key.join("")));
-        let bestKey = [...key];
-        let noImprovement = 0;
+        function runIterations() {
+            if (!evolutionRunning) return;
+            
+            let batchLimit = 500; // Process 500 iterations at a time before yielding to DOM
+            while (ctr < maxTries && batchLimit > 0) {
+                ctr++;
+                totalIterations++;
+                batchLimit--;
+                
+                let newKey = [...key];
+                const a = Math.floor(Math.random() * 26);
+                const b = Math.floor(Math.random() * 26);
+                [newKey[a], newKey[b]] = [newKey[b], newKey[a]];
+                
+                const score = ngramScore(decodeSubstitution(cleanText, newKey.join("")));
+                if (score > bestScore) {
+                    bestScore = score;
+                    key = [...newKey];
+                    bestKey = [...newKey];
+                    ctr = 0;
+                    
+                    if (bestScore > bestGlobalScore) {
+                        bestGlobalScore = bestScore;
+                        bestGlobalKey = bestKey.join("");
+                        bestGlobalDecryption = decodeSubstitution(message.toUpperCase(), bestGlobalKey);
+                        
+                        const timeElapsed = Date.now() - startTime;
+                        
+                        // Update UI
+                        const bestKeyEl = document.getElementById("bestKey");
+                        const bestKeyGenFoundEl = document.getElementById("bestKeyGenFound");
+                        const bestKeyIterationFoundEl = document.getElementById("bestKeyIterationFound");
+                        const bestKeyTimeFoundEl = document.getElementById("bestKeyTimeFound");
+                        const bestDecryptionEl = document.getElementById("bestDecryption");
 
-        // Perform small mutations on the key until no improvements are found after 2000 tries
-        while (noImprovement < 2000) {
-            noImprovement++;
-            let newKey = [...key];
-            
-            // Mutation: swap two random letters in the key mapping
-            const a = Math.floor(Math.random() * 26);
-            const b = Math.floor(Math.random() * 26);
-            [newKey[a], newKey[b]] = [newKey[b], newKey[a]];
-            
-            // Check if the mutated key decodes to a better quadgram score
-            const score = quadgramScore(decodeSubstitution(cleanText, newKey.join("")));
-            if (score > bestScore) {
-                bestScore = score;
-                key = [...newKey];
-                bestKey = [...newKey];
-                noImprovement = 0; // Reset counter since we found an improvement
+                        if(bestKeyEl) bestKeyEl.textContent = bestGlobalKey;
+                        if(bestKeyGenFoundEl) bestKeyGenFoundEl.textContent = currentRound + 1;
+                        if(bestKeyIterationFoundEl) bestKeyIterationFoundEl.textContent = totalIterations;
+                        if(bestKeyTimeFoundEl) bestKeyTimeFoundEl.textContent = timeElapsed + "ms";
+                        if(bestDecryptionEl) bestDecryptionEl.textContent = bestGlobalDecryption;
+                        
+                        // Append to result list
+                        const list = document.getElementById("evolutionResult");
+                        const li = document.createElement("li");
+                        li.style.padding = "10px";
+                        li.style.borderBottom = "1px solid rgba(255,255,255,0.1)";
+                        li.innerHTML = `<strong>Round ${currentRound + 1} | Iteration ${totalIterations}</strong> | Score: ${bestScore} | Time: ${timeElapsed}ms<br>
+                                        <span style="color: var(--primary-color)">Key: ${bestGlobalKey}</span><br>
+                                        <em>${bestGlobalDecryption.substring(0, 80)}...</em>`;
+                        list.prepend(li);
+                    }
+                }
+            }
+
+            if (ctr < maxTries) {
+                // If we hit batch limit but haven't reached max tries, yield and continue
+                requestAnimationFrame(runIterations);
+            } else {
+                // Finished round, proceed to next round
+                currentRound++;
+                startRound();
             }
         }
 
-        // If this round found a better absolute key, update the global UI
-        if (bestScore > bestGlobalScore) {
-            bestGlobalScore = bestScore;
-            bestGlobalKey = bestKey.join("");
-            bestGlobalDecryption = decodeSubstitution(message.toUpperCase(), bestGlobalKey);
-            
-            const bestKeyEl = document.getElementById("bestKey");
-            const bestKeyGenFoundEl = document.getElementById("bestKeyGenFound");
-            const bestKeyTimeFoundEl = document.getElementById("bestKeyTimeFound");
-            const bestDecryptionEl = document.getElementById("bestDecryption");
-
-            if(bestKeyEl) bestKeyEl.textContent = bestGlobalKey;
-            if(bestKeyGenFoundEl) bestKeyGenFoundEl.textContent = currentRound + 1;
-            if(bestKeyTimeFoundEl) bestKeyTimeFoundEl.textContent = (Date.now() - startTime) + "ms";
-            if(bestDecryptionEl) bestDecryptionEl.textContent = bestGlobalDecryption;
-        }
-
-        currentRound++;
-        requestAnimationFrame(runRound); // Schedule next round
-    }
-
-    runRound();
+        startRound();
+    });
 }
 
 function stopHillClimb() {
